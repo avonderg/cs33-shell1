cc439b0b9ad19e00a72d1e7652004079dc1ba730
avonderg <77714730+avonderg@users.noreply.github.com>
2021-10-31 16:20:28 -0400

    fixes

--- sh.c
+++ sh.c
@@ -294,7 +294,7 @@ int file_redirect(const char** input_file, const char** output_file, int* output
 //write descr
 // returns -1 if error, 1 if successful, 0 if none given
 int built_in(char *argv[512], char** path) {
-    if (strcmp(argv[0], "cd") ==0 && (strcmp(*path,argv[0]) == NULL)) { // if the command is cd
+    if (strcmp(argv[0], "cd") ==0 && (strcmp(*path,argv[0]) != 0)) { // if the command is cd
         // char *dir = argv[1];
          // pass in elt after 'cd'
         if (argv[1] == NULL) {
@@ -305,7 +305,7 @@ int built_in(char *argv[512], char** path) {
         }
         return 1;
     }
-    else if ((strcmp(argv[0], "ln") ==0) && (strcmp(*path,argv[0]) == NULL)) { // if the command is ln
+    else if ((strcmp(argv[0], "ln") ==0) && (strcmp(*path,argv[0]) != 0)) { // if the command is ln
         if (argv[1] == NULL)  {
             fprintf(stderr, "ln: syntax error");
         }
@@ -318,7 +318,7 @@ int built_in(char *argv[512], char** path) {
         }
         return 1;
     }
-    else if (strcmp(argv[0], "rm") ==0 && (strcmp(*path,argv[0]) == NULL)) { // if the command is rm
+    else if (strcmp(argv[0], "rm") ==0 && (strcmp(*path,argv[0]) != 0)) { // if the command is rm
         if (argv[1] == NULL)  {
             fprintf(stderr, "rm: syntax error");
         }
@@ -328,7 +328,7 @@ int built_in(char *argv[512], char** path) {
         }
         return 1;
     }
-    else if (strcmp(argv[0], "exit") ==0 && (strcmp(*path,argv[0]) == NULL)) { // if the command is exit
+    else if (strcmp(argv[0], "exit") ==0 && (strcmp(*path,argv[0]) != 0)) { // if the command is exit
         exit(0);
     }
     return 0;
