650fce33695f974a509127ff87e65bba7f98cc2c
avonderg <77714730+avonderg@users.noreply.github.com>
2021-10-31 15:28:31 -0400

    fixes

--- sh.c
+++ sh.c
@@ -204,9 +204,9 @@ int parse(char buffer[1024], char *tokens[512], char *argv[512], char *w_sym[512
             // input_index = i;
             *input_file = tokens[i+1];
             i+=2;
-            argv[k] = w_sym[i];
-            k++;
-            i++;
+            // argv[k] = w_sym[i];
+            // k++;
+            // i++;
             // cd /usr/bin/something 
             // get stuff after forward slash
         }
@@ -226,9 +226,9 @@ int parse(char buffer[1024], char *tokens[512], char *argv[512], char *w_sym[512
             // output_index = i;
             *output_file = tokens[i+1];
             i+=2;
-            argv[k] = w_sym[i];
-            k++;
-            i++;
+            // argv[k] = w_sym[i];
+            // k++;
+            // i++;
         }
         else if (strcmp(tokens[i],">>") == 0) {
             // error check first
@@ -246,9 +246,9 @@ int parse(char buffer[1024], char *tokens[512], char *argv[512], char *w_sym[512
             // output_index = i;
             *output_file = tokens[i+1];
             i+=2;
-            argv[k] = w_sym[i];
-            k++;
-            i++;
+            // argv[k] = w_sym[i];
+            // k++;
+            // i++;
         }
         else {  // otherwise, then add in element to argv
             argv[k] = w_sym[i]; 
